cf884d424f5948431bd2b51cfe743df0c4ae8438
Antônio José Teixeira <antoniojosegteixeira@gmail.com>
2022-02-21 10:22:09 -0400

    feat: add server connection

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import "dotenv/config";
+
+const app = express();
+
+app.use(cors());
+
+const CONNECTION_URL = process.env.MONGODB_URI;
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL)
+  .then((response) => {
+    app.listen(PORT, () => console.log("Server running on port", PORT));
+  })
+  .catch((err) => console.log(err.message));
