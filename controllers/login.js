a089026a9ae63d469fad3c7c05a6413aa31a53c9
Antônio José Teixeira <antoniojosegteixeira@gmail.com>
2022-02-22 03:48:00 -0400

    fix(login): get correct data from body

--- controllers/login.js
+++ controllers/login.js
@@ -2,19 +2,19 @@ import User from "../models/User.js";
 import bcrypt from "bcrypt";
 
 export const loginController = async (req, res) => {
-  const { email, password } = req.body;
+  const { email, password } = req.body.data;
 
   // Getting user
   const user = await User.findOne({ email }).select("+password");
 
   // Returning if user is not found
   if (!user) {
-    return res.status(400).send({ error: "User not found" });
+    return res.status(404).send({ error: "User not found" });
   }
 
   // Comparing passwords
   if (!(await bcrypt.compare(password, user.password))) {
-    return res.status(400).send({ error: "Invalid password" });
+    return res.status(401).send({ error: "Invalid password" });
   }
 
   // Returning user
