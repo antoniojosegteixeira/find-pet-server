6e33cbf40b94141f981e88395042d9059f5c26cd
Antônio José Teixeira <antoniojosegteixeira@gmail.com>
2022-02-22 01:06:14 -0400

    feat: check if registration email already exists

--- controllers/register.js
+++ controllers/register.js
@@ -1,3 +1,11 @@
-export const registerController = (req, res) => {
-  res.send("registered");
+import User from "../models/User.js";
+
+export const registerController = async (req, res) => {
+  /// Check if email already exists
+  const { email } = req.body;
+  if (await User.findOne({ email })) {
+    return res.send().status(409).send("Email already exists");
+  } else {
+    res.send().status(200);
+  }
 };
