d1f833017b0f5d09d9b0bedad608ec0cd4865878
Antônio José Teixeira <antoniojosegteixeira@gmail.com>
2022-02-22 08:56:34 -0400

    feat: add multer to routes

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,16 @@
 import express from "express";
+import multer from "multer";
 import { loginController } from "../controllers/login.js";
 import { registerController } from "../controllers/register.js";
 import { createPostController } from "../controllers/create-post.js";
 
 const router = express.Router();
+const upload = multer({ dest: "uploads/" });
 
 router.post("/login", loginController);
 
 router.post("/register", registerController);
 
-router.post("/create-post", createPostController);
+router.post("/create-post", upload.array("photos", 5), createPostController);
 
 export default router;
