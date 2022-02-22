5764f50a31f7ffe3db608893d94f18c3a287c411
Antônio José Teixeira <antoniojosegteixeira@gmail.com>
2022-02-22 01:31:47 -0400

    feat: add login controller

--- controllers/login.js
+++ controllers/login.js
@@ -1,3 +1,23 @@
-export const loginController = (req, res) => {
-  res.send("logged in");
+import User from "../models/User.js";
+import bcrypt from "bcrypt";
+
+export const loginController = async (req, res) => {
+  const { email, password } = req.body;
+
+  // Getting user
+  const user = await User.findOne({ email }).select("+password");
+
+  // Returning if user is not found
+  if (!user) {
+    return res.status(400).send({ error: "User not found" });
+  }
+
+  // Comparing passwords
+  if (!(await bcrypt.compare(password, user.password))) {
+    return res.status(400).send({ error: "Invalid password" });
+  }
+
+  // Returning user
+  user.password = undefined;
+  res.send({ user });
 };
