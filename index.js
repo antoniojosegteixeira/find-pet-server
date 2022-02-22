0c51e520e5be2a01b795826035aade2418242950
Antônio José Teixeira <antoniojosegteixeira@gmail.com>
2022-02-22 01:00:03 -0400

    fix: add url encoding to express

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ import "dotenv/config";
 import router from "./routes/index.js";
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(cors());
 
