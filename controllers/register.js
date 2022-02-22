8ee7703c67b06d3cc8b9e25511ab50ec21999729
Antônio José Teixeira <antoniojosegteixeira@gmail.com>
2022-02-22 05:54:52 -0400

    feat: register logs user in

--- controllers/register.js
+++ controllers/register.js
@@ -1,17 +1,21 @@
 import User from "../models/User.js";
-
+import { createUserToken } from "../functions/jwt.js";
 export const registerController = async (req, res) => {
   /// Check if user already exists
-  const { email } = req.body;
+  const { email } = req.body.data;
   if (await User.findOne({ email })) {
     return res.status(409).send("Email already exists");
   }
 
   // Creating user
-  const newUser = await User.create(req.body);
+  const newUser = await User.create(req.body.data);
 
   // Deleting password from schema
   newUser.password = undefined;
+
+  // Creating session token
+  const token = await createUserToken(newUser._id);
+  newUser.token = token;
 
   // Sending
   res.status(200).send(newUser);
