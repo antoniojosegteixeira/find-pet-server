e16dea118f9d3aef30323d95d1df0e9fd0412f25
Antônio José Teixeira <antoniojosegteixeira@gmail.com>
2022-02-22 01:25:50 -0400

    fix: correctly hash password

--- models/User.js
+++ models/User.js
@@ -21,11 +21,11 @@ const userSchema = mongoose.Schema(
 );
 
 // BCRYPT
-userSchema.pre("save", (next) => {
-  if (!this.isModified("password")) {
-    return next();
-  }
-  this.password = bcrypt.hashSync(this.password, 10);
+userSchema.pre("save", async function (next) {
+  const hash = await bcrypt.hash(this.password, 10);
+  this.password = hash;
+
+  next();
 });
 
 const User = mongoose.model("User", userSchema);
