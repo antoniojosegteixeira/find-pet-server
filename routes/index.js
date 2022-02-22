18169c7f84709b356c6d21a52411d5a44406a31d
Antônio José Teixeira <antoniojosegteixeira@gmail.com>
2022-02-22 08:48:59 -0400

    feat: add create post route

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,14 @@
 import express from "express";
 import { loginController } from "../controllers/login.js";
 import { registerController } from "../controllers/register.js";
+import { createPostController } from "../controllers/create-post.js";
 
 const router = express.Router();
 
 router.post("/login", loginController);
 
 router.post("/register", registerController);
+
+router.post("/create-post", createPostController);
 
 export default router;
